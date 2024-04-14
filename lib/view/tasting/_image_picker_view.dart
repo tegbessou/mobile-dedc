aed5fbd89be935d8005ecd95c1e2cc418cc8aad8
Tegbessou <hugues.gobet@gmail.com>
2024-04-14 14:14:50 +0200

    Add usage of gallery in picture

--- lib/view/tasting/_image_picker_view.dart
+++ lib/view/tasting/_image_picker_view.dart
@@ -1,5 +1,7 @@
 import 'dart:io';
 
+import 'package:degust_et_des_couleurs/view/_text_dm_sans.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
@@ -30,12 +32,67 @@ class ImagePickerViewState extends State<ImagePickerView> {
   @override
   Widget build(BuildContext context) {
     return IconButton(
-        onPressed: () => useCamera(ImageSource.camera),
+        onPressed: () => showCupertinoModalPopup(
+              context: context,
+              builder: (context) => CupertinoActionSheet(
+                actions: [
+                  CupertinoActionSheetAction(
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.image_outlined),
+                        Padding(
+                            padding: EdgeInsets.only(
+                              right: 10,
+                            ),
+                        ),
+                        TextDmSans(
+                          'Utiliser une photo existante',
+                          fontSize: 16,
+                          letterSpacing: 0,
+                        )
+                      ],
+                    ),
+                    onPressed: () {
+                      // close the options modal
+                      Navigator.of(context).pop();
+                      // get image from gallery
+                      useCamera(ImageSource.gallery);
+                    },
+                  ),
+                  CupertinoActionSheetAction(
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.camera_alt_outlined),
+                        Padding(
+                          padding: EdgeInsets.only(
+                            right: 10,
+                          ),
+                        ),
+                        TextDmSans(
+                          'Prendre une nouvelle photo',
+                          fontSize: 16,
+                          letterSpacing: 0,
+                        ),
+                      ],
+                    ),
+                    onPressed: () {
+                      // close the options modal
+                      Navigator.of(context).pop();
+                      // get image from camera
+                      useCamera(ImageSource.camera);
+                    },
+                  ),
+                ],
+              ),
+            ),
         icon: const Icon(Icons.photo_camera_back_outlined));
   }
 
   Future<void> useCamera(ImageSource source) async {
-    XFile? xFile = await imagePicker.pickImage(source: source);
+    XFile? xFile =
+        await imagePicker.pickImage(source: source, imageQuality: 75);
 
     if (xFile != null) {
       setFile(File(xFile.path));
